76c2659d3f2d92e00a2ae634a726cb3b38c482ef
donaldjvaldez <donaldj.valdez@gmail.com>
2025-09-17 02:47:23 -0400

    week02 Temples album

---
+++ scripts/temples.js
@@ -0,0 +1,17 @@
+
+const yearSpan = document.querySelector("#currentyear");
+const today = new Date();
+yearSpan.textContent = today.getFullYear();
+
+
+const lastModifiedSpan = document.querySelector("#lastModified");
+lastModifiedSpan.textContent = `Last Modified: ${document.lastModified}`;
+
+const mainnav = document.querySelector('.navigation')
+const hambutton = document.querySelector('#menu');
+
+// Add a click event listender to the hamburger button and use a callback function that toggles the list element's list of classes.
+hambutton.addEventListener('click', () => {
+    mainnav.classList.toggle('show');
+    hambutton.classList.toggle('show');
+});
