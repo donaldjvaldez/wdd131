059f81ef6995368110c809cc4014eea315ae06d1
donaldjvaldez <donaldj.valdez@gmail.com>
2025-09-22 17:55:08 -0400

    Let see the audit tool

---
+++ scripts/place.js
@@ -0,0 +1,7 @@
+const yearSpan = document.querySelector("#currentyear");
+const today = new Date();
+yearSpan.textContent = today.getFullYear();
+
+
+const lastModifiedSpan = document.querySelector("#lastModified");
+lastModifiedSpan.textContent = `Last Modified: ${document.lastModified}`;
