4543eee4063d1f329195c16c7b453999f0a7b8c6
donaldjvaldez <donaldj.valdez@gmail.com>
2025-09-22 18:53:39 -0400

    I think is done

--- scripts/place.js
+++ scripts/place.js
@@ -1,3 +1,26 @@
+// Static values based on Dominican Republic's climate
+const temperature = 29; // °C
+const windSpeed = 12;   // km/h
+const unit = "C";
+
+let windChill = "N/A";
+if (temperature <= 10 && windSpeed > 4.8) {// Determine if wind chill is applicable
+    windChill = calculateWindChill(temperature, windSpeed) + "°C";
+}
+
+
+// Display all weather info in order
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelector("#temperature").textContent = `${temperature}°${unit}`;
+    document.querySelector("#conditions").textContent = "Sunny";
+    document.querySelector("#windspeed").textContent = `${windSpeed} km/h`;
+    document.querySelector("#windchill").textContent = windChill;
+});
+
+
+
+
+//Footer//
 const yearSpan = document.querySelector("#currentyear");
 const today = new Date();
 yearSpan.textContent = today.getFullYear();
